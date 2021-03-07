412ead04fa31928585ac9186eeb7300b86a9dde2
DMibaf <58447025+DMibaf@users.noreply.github.com>
2021-03-07 03:41:16 -0100

    Added index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var data=null;
 // Em caso de sucesso
 function onFulfilled(children) {
     var data = children[0].children;
+    var style1 = ['border-primary', 'border-success', 'border-warning', 'border-danger'];
+    var style2 = ['bg-primary text-white', 'bg-success text-white', 'bg-warning text-white', 'bg-danger text-white'];
     if(data.length != 0){
         var i = 0;
+        var aux = '';
         for (child of data) {
             if(child.type == 'folder'){
                 i ++;
@@ -13,10 +16,19 @@ function onFulfilled(children) {
                 for (subChild of subChildren) {
                     pages = pages + '<p><a href="'+subChild.url+'">'+subChild.title+'</a></p>';
                 }
-                card = '<div class="col-3"><div class="card"><div class="card-header">'+child.title+'</div><div class="card-body">'+ pages +'</div></div></div>';
-                document.getElementById('app').innerHTML = document.getElementById('app').innerHTML + card;
+                card = '<div class="col-3"><div class="card '+style1[i-1]+'"><div class="card-header '+style2[i-1]+'">'+child.title+'</div><div class="card-body">'+ pages +'</div></div></div>';
+                if(i == 1){
+                    aux =  aux + '<div class="row">'+card;
+                }else if(i == 4){
+                    aux = aux + card + '</div>';
+                    i = 0;
+                }else{
+                    aux =  aux + card;
+                }
             }
         }
+        console.log(aux);
+        document.getElementById('app').innerHTML = document.getElementById('app').innerHTML + aux;
     }
 }
 
@@ -33,27 +45,3 @@ function  getlink(id){
 
 var gettingChildren = browser.bookmarks.getSubTree("unfiled_____");
 gettingChildren.then(onFulfilled, onRejected);
-
-
-
-
-
-/*
-if(children.length != 0){
-        var aux = '';
-        for (child of children) {
-            var card = '';
-            console.log(child.title);
-            if(child.type == 'folder'){
-                getlink(child.id).then(function(links){
-                    var pages = '';
-                    for (link of links) {
-                        pages = pages + '<p><a href="'+link.url+'">'+link.title+'</a></p>';
-                    }
-                    card = '<div class="col-3"><div class="card"><div class="card-header">'+child.title+'</div><div class="card-body">'+ pages +'</div></div></div>';
-                    document.getElementById('app').innerHTML = document.getElementById('app').innerHTML + card;
-                });
-            }
-        }
-    }
- */
